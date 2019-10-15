5dc3852b76598a1acbca5bdb5f2ad2744575b495
Wouter Kessels <w.kessels@student.tudelft.nl>
2019-10-15 13:22:14 +0200

    generalized ops

--- supports.py
+++ supports.py
@@ -1,13 +1,4 @@
-import os
-import shutil
 import numpy as np
-import matplotlib.pyplot as plt
-from datetime import datetime
-from scipy.signal import savgol_filter
-from itertools import chain, groupby
-from operator import itemgetter
-from glob import glob
-from copy import copy
 from qutip import *
 
 
@@ -26,13 +17,52 @@ psi_1pi = psi_pi
 psi_2pi = psi_0pi
 
 
-def ops(Nq, Nc):
-    # Qubit operators
-    b = tensor(destroy(Nq), qeye(Nc))
-    nq = b.dag()*b
-
-    # Cavity operators
-    a = tensor(qeye(Nq), destroy(Nc))
-    nc = a.dag()*a
+def ops(*args):
+    """
+    Returns system operators based on input arguments. The order
+    of the input arguments defines the convention of order of the
+    elements of the system (for example qubits and cavities).
     
-    return b, a, nq, nc
+    Input
+    -----
+    *args : int
+        Every argument represents the number of levels of a system element
+    
+    Returns
+    -------
+    a# : qutip.Qobj class object
+        Lowering operator for element #
+    n# : qutip.Qobj class object
+        Number operator for element #
+    """
+    if len(args) == 1:
+        a0 = destroy(args[0])
+        n0 = a0.dag()*a0
+        return a0, n0
+    
+    elif len(args) == 2:
+        a0 = destroy(args[0])
+        n0 = a0.dag()*a0
+        a1 = destroy(args[1])
+        n1 = a1.dag()*a1
+        return a0, a1, n0, n1
+    
+    elif len(args) == 3:
+        a0 = destroy(args[0])
+        n0 = a0.dag()*a0
+        a1 = destroy(args[1])
+        n1 = a1.dag()*a1
+        a2 = destroy(args[2])
+        n2 = a2.dag()*a2
+        return a0, a1, a2, n0, n1, n2
+    
+    elif len(args) == 4:
+        a0 = destroy(args[0])
+        n0 = a0.dag()*a0
+        a1 = destroy(args[1])
+        n1 = a1.dag()*a1
+        a2 = destroy(args[2])
+        n2 = a2.dag()*a2
+        a3 = destroy(args[3])
+        n3 = a3.dag()*a3
+        return a0, a1, a2, a3, n0, n1, n2, n3
