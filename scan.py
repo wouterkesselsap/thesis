71432ed429ae52c232f3733776be44c420d7c691
wouterkesselsap <w.kessels@student.tudelft.nl>
2019-11-23 09:09:09 +0100

    restored qfs and cfs function

--- scan.py
+++ scan.py
@@ -12,7 +12,7 @@ from IPython.display import display
 
 
 def sbsample(Nq, wq, wc, Ec, g, shift, sb, Nt, H, H_args, psi0, Np_per_batch,
-             options, home, parallel, *args, **kwargs):
+             options, home, parallel, *args):
     """
     Performs a single- or double-tone sideband transition simulation
     with the given input parameters. Plots the expectation
@@ -130,7 +130,6 @@ def sbsample(Nq, wq, wc, Ec, g, shift, sb, Nt, H, H_args, psi0, Np_per_batch,
     figqc.savefig(home + "temp/figqc{}_{}.png".format(num, shift/2/pi))
     plt.close(fig)
     plt.close(figqc)
-    
     return figqc, fig
 
 
@@ -249,3 +248,68 @@ def sbsample_visualize_sweep(Nq, wq, wc, Ec, g, shift, sb, Nt, H, H_args, psi0,
     plt.close(fig)
     plt.close(figqc)
     return figqc, fig
+
+
+def qfs(Nq, wq, Ec, wp, H, H_args, psi0, Nc, Np, Np_per_batch, options, home, parallel):
+    """
+    Applies a probe tone to an uncoupled qubit to find its transition frequency,
+    which can be shifted due to a dispersive drive.
+    
+    Input
+    -----
+    The input parameters are equal to the names in 2p_sideband.ipynb.
+    """
+    i = wp[0]
+    wp = wp[1]
+    H_args['wp'] = wp
+    b, nq = ops(Nq)
+    e_ops = [nq]
+        
+    srcfolder = calculate(H, psi0, e_ops, H_args, options, Nc, Np, Np_per_batch,
+                          home, parallel, verbose=False)
+    quants = ['times', 'expect']
+    combine_batches(srcfolder, quants=quants, return_data=False)
+    times, states, expect, e0, g1, e1, g0, coupling = load_data(quants, srcfolder)
+    
+    if i < 10:
+        num = "0" + str(i)
+    elif i >= 10:
+        num = str(i)
+    
+    print("\nshift  =", (wp-wq)/2/pi)
+    print("wp     =", wp/2/pi)
+    print("max    =", max(expect[0]))
+    
+    plt.figure(figsize=[15,3])
+    plt.plot(times, expect[0], c='k')
+    plt.title("shift {}, wp {}, max {}".format(np.round((wp-wq)/2/pi,4), np.round(wp/2/pi,4), np.round(max(expect[0]),4)))
+    plt.savefig(home + "temp/fig{}_{}.png".format(num, (wp-wq)/2/pi))
+    
+
+def cfs(Nq, Nc, wc, Ec, wp, H, H_args, psi0, Np_per_batch, options, home, parallel):
+    i = wp[0]
+    wp = wp[1]
+    H_args['wp'] = wp
+    Np = 100*int(H_args['t3'])     # number of discrete time steps for which to store the output
+    b, a, nq, nc = ops(Nq, Nc)  # Operators
+    e_ops = [nq, nc]
+    
+    srcfolder = calculate(H, psi0, e_ops, H_args, options, Nc, Np, Np_per_batch,
+                          home, parallel, verbose=False)
+    quants = ['times', 'expect']
+    combine_batches(srcfolder, quants=quants, return_data=False)
+    times, states, expect, e0, g1, e1, g0, coupling = load_data(quants, srcfolder)
+    
+    if i < 10:
+        num = "0" + str(i)
+    elif i >= 10:
+        num = str(i)
+    
+    print("wp     =", wp/2/pi)
+    print("max    =", max(expect[1]))
+    
+    plt.figure(figsize=[15,3])
+    plt.plot(times, expect[0], c='b')
+    plt.plot(times, expect[1], c='r')
+    plt.title("shift {}, wp {}, max {}".format(np.round((wp-wc)/2/pi,4), np.round(wp/2/pi,4), np.round(max(expect[1]),6)))
+    plt.savefig(home + "temp/fig{}_{}.png".format(num, (wp-wc)/2/pi))
